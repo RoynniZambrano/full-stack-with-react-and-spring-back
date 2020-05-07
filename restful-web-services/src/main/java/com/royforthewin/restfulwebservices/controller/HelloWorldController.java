4c34721d5c1b5ca0c2d242c447e8530d54dc9d5c
Roy <roynny51@gmail.com>
2020-05-06 22:29:56 -0300

    section 6

--- restful-web-services/src/main/java/com/royforthewin/restfulwebservices/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/com/royforthewin/restfulwebservices/controller/HelloWorldController.java
@@ -2,11 +2,13 @@ package com.royforthewin.restfulwebservices.controller;
 
 import com.royforthewin.restfulwebservices.model.HelloWorldBean;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController {
 
 
